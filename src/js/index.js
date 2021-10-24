b2d75f04d58e81779c09da61cc3894eab3831c1c
OualidZM <ozaaj@cifpfbmoll.eu>
2021-10-24 20:00:18 +0200

    docs: :construction: js
    correcting the delete button(  'C', 'CE' ), and improving performance on the shift  button. I started to add too the send button and the gift button

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,10 @@ var contadorShift = 0;
 var arrBtn = document.querySelectorAll('.btn');
 arrBtn.forEach(element => element.addEventListener('click',(e)=>{
 
-    if(element.innerHTML === 'space'){
+    if(element.innerHTML === 'SPACE'){
         return document.getElementById('keyValue').value += ' ';
     }
-    else if(element.innerHTML === 'enterr'){
+    else if(element.innerHTML === 'enter'){
         return document.getElementById('keyValue').value += "\n";
     }
 
@@ -23,34 +23,36 @@ arrBtn.forEach(element => element.addEventListener('click',(e)=>{
     }
     else if(element.innerHTML === 'C'){
         let inputValue = document.getElementById('keyValue').value
-        // let arr = inputValue.split(' ');
-        // let result = arr.pop();
-        return document.getElementById('keyValue').value  = document.getElementById('keyValue').value.substr(0, inputValue.lastIndexOf(" ")); 
+        return document.getElementById('keyValue').value  = document.getElementById('keyValue').value.replace(inputValue,'');
+
 
     }
     else if(element.innerHTML === 'CE'){
         let inputValue = document.getElementById('keyValue').value
-        return document.getElementById('keyValue').value  = document.getElementById('keyValue').value.replace(inputValue,'');
+        return document.getElementById('keyValue').value  = document.getElementById('keyValue').value.substr(0, inputValue.lastIndexOf(" ")); 
     }
     else if(element.innerHTML === 'shift'){
         contadorShift++;
         let shift = document.querySelector('.shiftStyle')
 
-        if(contadorShift != 3){
-            shift.style.backgroundColor = 'red';
-            shift.style.border = 'red';
+        if(contadorShift == 1){
+            shift.style.backgroundColor = 'cyan';
             arrBtn.forEach(e => e.style.textTransform = 'uppercase');
-    
+        }
+
+        else if(contadorShift == 2){
+            shift.style.border = 'thick solid black';
+
         }else{
-            shift.style.backgroundColor = 'green';
-            shift.style.border = 'green';
+            shift.style.backgroundColor = '#e9e9ed';
+            shift.style.border = '#e9e9ed';
             arrBtn.forEach(e => e.style.textTransform = 'lowercase');
+            contadorShift = 0;
         }
     }   
 
     else{
             let shift = document.querySelector('.shiftStyle')
-
             if(contadorShift == 0){
                 return document.getElementById('keyValue').value += element.innerHTML.toLowerCase();
             }
@@ -61,12 +63,28 @@ arrBtn.forEach(element => element.addEventListener('click',(e)=>{
                 contadorShift = 0;
             }
             else if(contadorShift == 2){
+
                 return document.getElementById('keyValue').value += element.innerHTML.toUpperCase();
             }
             else{
                 if(contadorShift == 3){
-                contadorShift = 0;
                 return document.getElementById('keyValue').value += element.innerHTML.toLowerCase();
                 }
             }
     }}));
+
+document.getElementById('giftButton').addEventListener('click',()=>{
+    alert('Gift Button')
+})
+
+document.getElementById('sendButton').addEventListener('click',()=>{
+    let inputValue = document.getElementById('keyValue').value //input value
+    let areaMessage =  document.querySelector('.messageArea') ; // area message
+    let createParagraf = document.createElement('p'); // create p element
+    
+    let appendChild = areaMessage.appendChild(createParagraf);
+    appendChild.innerHTML = document.getElementById('keyValue').value;
+    appendChild.className = 'message'
+
+    document.getElementById('keyValue').value  = document.getElementById('keyValue').value.replace(inputValue,'');
+})
